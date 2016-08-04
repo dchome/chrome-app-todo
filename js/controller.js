fb34afda36d007dfe2595a71d3b3c3e8430b9267
Daniel Homer <daniel.homer6@gmail.com>
2016-08-04 15:36:11 -0400

    fix bug regarding getting todo count

--- js/controller.js
+++ js/controller.js
@@ -235,16 +235,17 @@
    * number of todos.
    */
   Controller.prototype._updateCount = function () {
-    var todos = this.model.getCount();
+    this.model.getCount(function(todos){
 
-    this.$todoItemCounter.innerHTML = this.view.itemCounter(todos.active);
+      this.$todoItemCounter.innerHTML = this.view.itemCounter(todos.active);
 
-    this.$clearCompleted.innerHTML = this.view.clearCompletedButton(todos.completed);
-    this.$clearCompleted.style.display = todos.completed > 0 ? 'block' : 'none';
+      this.$clearCompleted.innerHTML = this.view.clearCompletedButton(todos.completed);
+      this.$clearCompleted.style.display = todos.completed > 0 ? 'block' : 'none';
 
-    this.$toggleAll.checked = todos.completed === todos.total;
+      this.$toggleAll.checked = todos.completed === todos.total;
 
-    this._toggleFrame(todos);
+      this._toggleFrame(todos);
+    }.bind(this));
   };
 
   /**
